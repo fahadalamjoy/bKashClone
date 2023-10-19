d053ac86717e63be10a8de60179ce8fdafd076c7
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2023-10-19 16:12:18 +0600

    bottom tab done

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { View, StatusBar } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -10,12 +11,8 @@ import Home from "./screen/Home";
 import Transition from "./screen/Transition";
 import ScreenOne from "./screen/addMoney/ScreenOne";
 import ScreenTwo from "./screen/addMoney/ScreenTwo";
-
-// import Home from "./screen/Home";
-// import Transition from "./screen/Transition";
-// import ScreenOne from "./screen/addMoney/ScreenOne";
-// import ScreenTwo from "./screen/addMoney/ScreenTwo";
-// import ScreenThree from "./screen/addMoney/ScreenThree";
+import ScanQr from "./screen/ScanQr";
+import Inbox from "./screen/Inbox";
 
 const Tab = createBottomTabNavigator();
 const HomeStack = createStackNavigator();
@@ -24,17 +21,93 @@ const Drawer = createDrawerNavigator();
 function HomeStackGroup() {
   return (
     <HomeStack.Navigator initialRouteName="TabsGroup">
-      <HomeStack.Screen name="TabsGroup" component={TabsGroup} />
-      <HomeStack.Screen name="ScreenThree" component={ScreenThree} />
+      <HomeStack.Screen
+        name="TabsGroup"
+        component={TabsGroup}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="ScreenThree"
+        component={ScreenThree}
+        options={{ headerShown: false }}
+      />
     </HomeStack.Navigator>
   );
 }
 
 function TabsGroup() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Settings" component={Transition} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+        tabBarStyle: {
+          paddingVertical: 10,
+        },
+        tabBarIconStyle: {
+          marginTop: -10,
+        }
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={size} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Scan"
+        component={ScanQr}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <View
+              style={{
+                alignItems: "center",
+                
+                borderColor: "red", 
+                padding: 50, // Border color
+              }}
+            >
+              <View style={{ 
+                alignItems: "center",
+                position: "absolute",
+                backgroundColor: "gray",
+                padding: 10,
+                }}>
+
+              <AntDesign name="qrcode" size={45} color={color} 
+                style={{
+                  backgroundColor: "white",
+                  position: "absolute",
+                  top: -25,
+                  alignContent: "center",
+                  alignItems: "center",
+                  padding: 20,
+                  borderRadius: 50,
+                  alignItems: "center",
+                  borderRadius: 50,
+                  borderWidth: 2,
+                  borderColor: "red",
+                }}
+                />
+                </View>
+            </View>
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Transition}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="inbox" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -42,9 +115,21 @@ function TabsGroup() {
 function DrawerGroup() {
   return (
     <Drawer.Navigator initialRouteName="Feed">
-      <Drawer.Screen name="Feed" component={HomeStackGroup} />
-      <Drawer.Screen name="ScreenOne" component={ScreenOne} />
-      <Drawer.Screen name="ScreenTwo" component={ScreenTwo} />
+      <Drawer.Screen
+        name="Feed"
+        component={HomeStackGroup}
+        options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+        name="ScreenOne"
+        component={ScreenOne}
+        options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+        name="ScreenTwo"
+        component={ScreenTwo}
+        options={{ headerShown: false }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -52,8 +137,8 @@ function DrawerGroup() {
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerGroup />
       <StatusBar style="auto" />
+      <DrawerGroup />
     </NavigationContainer>
   );
 }
